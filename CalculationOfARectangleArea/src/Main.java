a72e08e77346119212730da793d4119064786185
Valentyn <147154747+horeyzer@users.noreply.github.com>
2023-10-27 13:24:14 +0200

    Add files via upload

---
+++ CalculationOfARectangleArea/src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        RectangleArea rect = new RectangleArea();
+        rect.getData();
+        rect.fieldDisplay();
+    }
+}
---
+++ CalculationOfARectangleArea/src/RectangleArea.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class RectangleArea
+{
+    double length;
+    double width;
+
+    public void getData()
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter length of the rectangle in cm: ");
+        length = scan.nextDouble();
+
+        System.out.print("Enter width of the rectangle in cm: ");
+        width = scan.nextDouble();
+    }
+
+    public double computeField()
+    {
+        return length * width;
+    }
+
+    public void fieldDisplay()
+    {
+        System.out.println("Rectangle Information: ");
+        System.out.println("Length: " + length);
+        System.out.println("Width:" + width);
+        System.out.println("Area: " + computeField());
+    }
+}
